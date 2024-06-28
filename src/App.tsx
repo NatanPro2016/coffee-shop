ea1447dade619da70aa802830207b37915af0ed0
natanpro2016 <blacknatan2004@gmail.com>
2024-06-28 10:37:59 +0100

    simple changes on ui

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ import Products from "./components/Products";
 
 function App() {
   return (
-    <main className="text-[#afa18f]">
+    <main className="text-[#FEF7EE]">
       <Banner />
-      <div className="xl:w-[1150px] w-5/6 m bg-[#1B1D1F] mx-auto z-10 rounded-xl mt-[-150px] relative py-[50px] flex flex-col items-center">
+      <div className="xl:w-[1000px] w-5/6 bg-[#1B1D1F] mx-auto z-10 rounded-xl mt-[-140px] relative py-[70px] flex flex-col items-center">
         <Hero />
         <Products />
       </div>
